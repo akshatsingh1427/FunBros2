67f528f250bbcd9da288f3d76764f95809fc78f4
akshatsingh1427 <singhakshatamlesh@gmail.com>
2026-02-21 21:06:38 +0530

    Remove comment from _process function
    Remove comment about ESC key functionality.

--- main_menu_3.gd
+++ main_menu_3.gd
@@ -7,7 +7,6 @@ func _on_level_3_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game_3.tscn")
 
 func _process(_delta):
-	# ESC always exits from menu
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
